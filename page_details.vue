d8f12a1e55ae2b2244541bc7cf34bbb6e55604e8
stephanie@mobilefringe.com @ CodeCloud <stephanie@mobilefringe.com @ CodeCloud>
2019-05-30 11:18:54 -0400

    Redlands Village 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -3,7 +3,7 @@
         <loading-spinner v-if="!dataLoaded"></loading-spinner>
         <transition name="fade">
             <div v-if="dataLoaded" v-cloak>
-                <div class="inside_header_background" :style="{ background: 'linear-gradient(0deg, rgba(204, 80, 56, 0.8), rgba(204, 80, 56, 0.8)), url(' + pageBanner.image_url + ') center center no-repeat rgba(204, 80, 56)' }">
+                <div class="inside_header_background" :style="{ background: 'linear-gradient(0deg, rgba(150,61,43, 0.8), rgba(150,61,43, 0.8)), url(' + pageBanner.image_url + ') center center no-repeat rgba(150,61,43)' }">
                     <div class="main_container">
                         <h2 v-if="currentPage.title" v-html="currentPage.title"></h2>
                     </div>
