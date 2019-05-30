ae96098880fab1fcdfe1aeeee1190e6caa1b7b92
stephanie@mobilefringe.com @ CodeCloud <stephanie@mobilefringe.com @ CodeCloud>
2019-05-30 09:35:25 -0400

    Redlands Village 1.0  /inside_banner.vue

--- inside_banner.vue
+++ inside_banner.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="inside_header_background" v-if="pageBanner" :style="{ background: 'linear-gradient(0deg, rgba(204, 80, 56, 0.8), rgba(251, 129, 105, 0.8)), url(' + pageBanner.image_url + ') center center no-repeat rgba(204, 80, 56)' }">
+    <div class="inside_header_background" v-if="pageBanner" :style="{ background: 'linear-gradient(0deg, rgba(204, 80, 56, 0.8), rgba(204, 80, 56, 0.8)), url(' + pageBanner.image_url + ') center center no-repeat rgba(204, 80, 56)' }">
         <div class="main_container">
             <div class="page_container">
                 <h2 v-html="pageName"></h2>
